d56c0743b2a696c98f29bcb740c44ead02911afa
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-02-07 14:59:29 -0500

    project setup

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '@/assets/styles/globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'BuildBin',
+  description: 'E-commerce platform for selling and buying hobby materials.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}`}>{children}</body>
+    </html>
+  );
+}
