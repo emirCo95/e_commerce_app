a19a41565adbda6e1250a0fbcfc0312a9b91e010
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-02-09 15:18:34 -0500

    create prisma actions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,11 @@
-import sampleData from '@/db/sample-data';
+import { getLatestProducts } from '@/lib/actions/product.actions';
 import ProductList from '@/components/shared/product/product-list';
 
-const Home = () => {
+const Home = async () => {
+  const latestProducts = await getLatestProducts();
   return (
     <>
-      <ProductList
-        data={sampleData.products}
-        title="Newest Arrivals"
-        limit={4}
-      />
+      <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
     </>
   );
 };
